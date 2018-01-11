7486a5483ba5d53d95c4cea64789399825b17848
Ivan Sidorenko <i.sidorenko@plarium.com>
2018-01-11 17:16:31 +0200

    Updates

--- src/redux/store.js
+++
@@ -1,5 +0,0 @@
-import { createStore, applyMiddleware } from 'redux';
-import dataReducer from './reducer';
-import promiseMiddleware from 'redux-promise';
-
-export default createStore(dataReducer, applyMiddleware(promiseMiddleware));
