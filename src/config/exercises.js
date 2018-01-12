31b1e0d30cb75b52808aede1ee5861d731a5ebd7
Ivan Sidorenko <i.sidorenko@plarium.com>
2018-01-12 13:10:53 +0200

    Change default exercise to be shown on github

--- src/config/exercises.js
+++ src/config/exercises.js
@@ -1,13 +1,13 @@
 export default [{
-  id: 'orbitrack',
-  name: 'Orbitrack',
-  results: ['calories'],
-  params: ['calories']
-}, {
   id: 'pushups',
   name: 'Push ups',
   results: ['number'],
   params: ['totalNumber']
+}, {
+  id: 'orbitrack',
+  name: 'Orbitrack',
+  results: ['calories'],
+  params: ['calories']
 }, {
   id: 'benchPress',
   name: 'Bench Press',
